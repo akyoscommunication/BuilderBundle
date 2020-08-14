b9b5e291cafd1093841493587ca1fb10a0b2e718
Alexis <alexis.bouchard.akyos@gmail.com>
2020-08-14 10:12:58 +0200

    fix: remove controller forward

--- Service/Builder.php
+++ Service/Builder.php
@@ -13,14 +13,11 @@ use Akyos\BuilderBundle\Form\MakeTemplateType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
-use Gedmo\Translatable\Entity\Translation;
 use Gedmo\Translatable\Query\TreeWalker\TranslationWalker;
 use Psr\Container\ContainerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment;
 
