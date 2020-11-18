28b14ea2baf0ccde1ea1b2f841045fc60a54276e
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2020-11-18 17:16:34 +0100

    Fix button

--- assets/scripts/components/Button.js
+++ assets/scripts/components/Button.js
@@ -11,7 +11,7 @@ class Button {
 				const href = $(this).attr('href');
 				const anchor = $(href);
 				anchor.prepend('<div class="'+href+'" style="position: absolute;top:-40px;"></div>');
-				anchor.style('position', 'relative');
+				anchor.css('position', 'relative');
 				$(this).click(function(e) {
 					e.preventDefault();
 					$("html, body").stop().animate( { scrollTop: anchor.offset().top }, 1500);
