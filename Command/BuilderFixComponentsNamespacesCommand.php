ef8b88823eecb6e306ab2f9272249dd1e63b61cd
Lilian Carneau <lilian.akyos@gmail.com>
2020-12-01 16:53:42 +0100

    fix: style and more

---
+++ Command/BuilderFixComponentsNamespacesCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Akyos\BuilderBundle\Command;
+
+use Akyos\BuilderBundle\Entity\Component;
+use Akyos\CoreBundle\Twig\CoreExtension;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class BuilderFixComponentsNamespacesCommand extends Command
+{
+    protected static $defaultName = 'builder:fix-components-namespaces';
+    /** @var EntityManagerInterface */
+    private $em;
+    /** @var CoreExtension */
+    private CoreExtension $coreExtension;
+
+    public function __construct(string $name = null, EntityManagerInterface $em, CoreExtension $coreExtension)
+    {
+        parent::__construct($name);
+        $this->em = $em;
+        $this->coreExtension = $coreExtension;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Fix les namespaces des components pour les mettres en namespace complet.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $components = $this->em->getRepository(Component::class)->findAll();
+
+        foreach ($components as $component) {
+            $initType = $component->getType();
+
+            if (!class_exists($initType)) {
+                $type = $this->coreExtension->getEntityNameSpace($initType);
+                $component->setType($type);
+                $this->em->flush();
+            }
+        }
+
+        $io->success('Changement terminé.');
+
+        return 0;
+    }
+}
--- Controller/ViewController.php
+++ Controller/ViewController.php
@@ -2,8 +2,14 @@
 
 namespace Akyos\BuilderBundle\Controller;
 
+use Akyos\BuilderBundle\AkyosBuilderBundle;
+use Akyos\BuilderBundle\Entity\BuilderOptions;
 use Akyos\BuilderBundle\Entity\ComponentTemplate;
+use Akyos\BuilderBundle\Form\SubmitBuilderType;
 use Akyos\BuilderBundle\Repository\ComponentRepository;
+use Akyos\CoreBundle\Entity\Page;
+use Akyos\CoreBundle\Services\CoreService;
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,20 +33,52 @@ class ViewController extends AbstractController
      *
      * @param Filesystem $filesystem
      * @param KernelInterface $kernel
+     * @param CoreService $coreService
+     * @param ContainerInterface $container
      * @return Response
      */
-    public function view($type, $typeId, ComponentRepository $componentRepository, Request $request, Filesystem $filesystem, KernelInterface $kernel): Response
+    public function view($type, $typeId, ComponentRepository $componentRepository, Request $request, Filesystem $filesystem, KernelInterface $kernel, CoreService $coreService, ContainerInterface $container): Response
     {
         $type = urldecode($type);
         $em = $this->getDoctrine()->getManager();
         $el = $em->getRepository($type)->find($typeId);
         $array = explode('\\', $type);
         $entity = end($array);
-        $components = $componentRepository->findBy(['type' => $entity, 'typeId' => $typeId, 'isTemp' => true, 'parentComponent' => null], ['position' => 'ASC']);
+        $checkBundle = $coreService->checkIfBundleEnable(AkyosBuilderBundle::class, BuilderOptions::class, $type);
+
+        $array = [
+            'andWhere' => [
+                'published' => true,
+                'props' => 'value'
+            ],
+            'orWhere' => [
+                'published' => true,
+                'props' => 'value'
+            ],
+        ];
+
+        $form = $this->createForm(SubmitBuilderType::class);
+        $form->handleRequest($request);
+        if ($checkBundle) {
+            if (!$form->isSubmitted()) {
+                $container->get('render.builder')->initCloneComponents($type, $el->getId());
+            }
+        }
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($checkBundle) {
+                $container->get('render.builder')->tempToProd($type, $el->getId());
+            }
+            $em->flush();
+
+            return $this->redirect($request->getUri());
+        } elseif ($form->isSubmitted() && !($form->isValid())) {
+            throw $this->createNotFoundException("Formulaire invalide.");
+        }
+
+        $components = $componentRepository->findBy(['type' => $type, 'typeId' => $typeId, 'isTemp' => true, 'parentComponent' => null], ['position' => 'ASC']);
         $componentTemplates = $em->getRepository(ComponentTemplate::class)->findAll();
 
-        $view = '';
-        if ($entity === "Page") {
+        if ($type === Page::class) {
             $view = $el->getTemplate() ? '/page/'.$el->getTemplate().'.html.twig' : '@AkyosCore/front/content.html.twig';
         } else {
             $view = $filesystem->exists($kernel->getProjectDir()."/templates/${entity}/single.html.twig")
@@ -56,6 +94,7 @@ class ViewController extends AbstractController
             'typeId' => $typeId,
             'edit' => true,
             'first' => true,
+            'form' => $form->createView(),
         ]);
     }
 }
